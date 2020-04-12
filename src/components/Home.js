e39be0fa48e6c3621be851348b4bb18c74cf7b27
Igor Rozum <irozum@greengeeks.com>
2020-04-12 17:08:14 -0400

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, TouchableHighlight} from 'react-native';
+import {View, Text, StyleSheet, TouchableHighlight, TouchableOpacity} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import {BlurView} from '@react-native-community/blur';
 
 export default function Home({navigation}) {
   const [score, setScore] = useState(0);
   const [continent, setContinent] = useState('All Continents');
+  const [continentsSelect, setContinentsSelect] = useState(false);
+  const [languagesSelect, setLanguagesSelect] = useState(false);
 
   const getScore = async () => {
     try {
@@ -20,11 +22,45 @@ export default function Home({navigation}) {
     getScore();
   }, []);
 
+  let selectView = null;
+  if (continentsSelect) {
+    selectView = (
+      <View style={styles.absolute}>
+        <BlurView
+          style={styles.absolute}
+          blurType="light"
+          blurAmount={20}
+          reducedTransparencyFallbackColor="white"
+        />
+        <View style={styles.blurView}>
+          <TouchableHighlight
+            onPress={() => setMenuOpen(false)}
+            style={styles.selectViewButtons}>
+            <Text style={styles.btnText}>Resume</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => handleRestart()}
+            style={styles.selectViewButtons}>
+            <Text style={styles.btnText}>Restart</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => navigation.reset({routes: [{name: 'Home'}]})}
+            style={styles.selectViewButtons}>
+            <Text style={styles.btnText}>Main Menu</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.topArea}>
         <Text style={styles.scoreText}>Highest Score</Text>
         <Text style={styles.score}>{score}</Text>
+        <TouchableOpacity onPress={() => setContinentsSelect(true)}>
+          <Text>{continent}</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.buttonsView}>
         <TouchableHighlight
@@ -34,6 +70,7 @@ export default function Home({navigation}) {
         </TouchableHighlight>
       </View>
       <View style={styles.adView} />
+      {selectView}
     </View>
   );
 }
@@ -76,5 +113,25 @@ const styles = StyleSheet.create({
   adView: {
     flex: 2,
     // backgroundColor: 'red',
+  },
+  absolute: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  blurView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  selectViewButtons: {
+    backgroundColor: 'black',
+    width: '65%',
+    paddingVertical: 10,
+    marginVertical: 20,
+    borderRadius: 50,
+    alignItems: 'center',
   },
 });
