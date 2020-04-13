270f00f3deff4c584662593571d103b3b41f8b6d
Igor Rozum <irozum@greengeeks.com>
2020-04-13 18:04:02 -0400

    xcode build

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from './src/components/Home';
 import Game from './src/components/Game';
 import {StatusBar} from 'react-native';
-// import Icon from 'react-native-vector-icons/MaterialIcons'
-
-// Icon.loadFont('FontAwesome');
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createStackNavigator();
 StatusBar.setHidden(true);
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
